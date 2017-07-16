b5082785663771ea53ec05185459f3657ba2b07a
skiming <skiming@126.com>
2017-07-16 22:46:15 +0800

    update

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 * @Author: skiming
 * @Date:   2017-07-16 22:00:55
 * @Last Modified by:   skiming
-* @Last Modified time: 2017-07-16 22:09:49
+* @Last Modified time: 2017-07-16 22:46:03
 */
 
 import './test.css';
